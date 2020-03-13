cad8deca232a2db10abad3dae6d29bcf529b11a8
Hafiz Umer Bin Nasir <umer.nasir@aku.edu>
2020-03-13 16:54:42 +0500

    user roles query

--- ComplainPortal/Controllers/AccountController.cs
+++ ComplainPortal/Controllers/AccountController.cs
@@ -1,12 +1,16 @@
 ﻿using ComplainPortal.Models;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
 using System.DirectoryServices.Protocols;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using System.Xml.Linq;
+
+
 
 namespace ComplainPortal.Controllers
 {
@@ -21,14 +25,12 @@ namespace ComplainPortal.Controllers
 
         public JsonResult Login(Login userlogin)
         {
-            var flag = false;
-            //checking xml
-            var userrole=  CheckUsersRoleFromXml(userlogin.email);
-
-           
+            bool flag = false;
             //calling ldap validation function to validate user
            // returns true if exist 
             flag = validateUserByBind(userlogin.email, userlogin.password);
+            //checking xml
+            CheckUsersRoleFromXml(userlogin.email);
             //sending response to ajax 
             return Json(new { Login = flag,statuscode=200 }, JsonRequestBehavior.AllowGet);
         }
@@ -40,10 +42,13 @@ namespace ComplainPortal.Controllers
             var credentials = new NetworkCredential(username, password);
             var serverId = new LdapDirectoryIdentifier("akudc14.aku.edu");
 
+            
             var conn = new LdapConnection(serverId, credentials);
             try
             {
                 conn.Bind();
+
+                Getuserinfo(username);
             }
             catch (Exception)
             {
@@ -55,11 +60,27 @@ namespace ComplainPortal.Controllers
             return result;
         }
 
+
+        public void Getuserinfo(string username)
+        {
+            using (var context = new PrincipalContext(ContextType.Domain, "akudc14.aku.edu"))
+            {
+                var user = UserPrincipal.FindByIdentity(context, username);
+                if (user != null)
+                {
+                    ViewBag.UserName = user.Name;
+                    ViewBag.EmailAddress = user.HomeDirectory;
+                }
+
+
+            }
+
+        }
         #endregion
 
         #region Xml File Parsing
 
-        public bool CheckUsersRoleFromXml(string username)
+        public void CheckUsersRoleFromXml(string username)
         {
             //loading xml file to xml dosument object
             XDocument xml = XDocument.Load(HttpContext.Server.MapPath("/Utilities/Users.xml"));
@@ -71,16 +92,22 @@ namespace ComplainPortal.Controllers
                 {
                     //getting values from node
                     var name = user.Element("name").Value.ToString();
-                    var age = user.Element("role").Value.ToString();
-
-                    //matching 
+                    //matching user name with xml users 
                     if (name == username)
                     {
-                        return true;
+                        //array list for storing roles
+                        ArrayList roles = new ArrayList();
+                        foreach(var userroles in user.Descendants("role"))
+                        {
+                            roles.Add(userroles.Value.ToString());
+                        }
+                        Session["userroles"] = roles;
                     }
+
                 }
+                
             }
-            return false;
+            
         }
 
         #endregion
--- ComplainPortal/Controllers/DashboardController.cs
+++ ComplainPortal/Controllers/DashboardController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
@@ -21,31 +22,62 @@ namespace ComplainPortal.Controllers
             return Json(new { statuscode=200,success=true}, JsonRequestBehavior.AllowGet);
         }
 
-        public bool SendEmailWithSmtp()
+        public void SendEmailWithSmtp()
         {
-            try
+            using (SmtpClient smtpClient = new SmtpClient())
             {
+                var basicCredential = new NetworkCredential("umer.nasir@aku.edu", "");
+                using (MailMessage message = new MailMessage())
+                {
+                    MailAddress fromAddress = new MailAddress("umer.nasir@aku.edu");
 
+                    smtpClient.UseDefaultCredentials = false;
+                    smtpClient.Host = "exmbxcas4.aku.edu";
+                    
+                    smtpClient.Credentials = basicCredential;
+                    smtpClient.Port = 25;
 
-                MailMessage mail = new MailMessage();
-                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
-               
-                mail.From = new MailAddress("AEMS_NotificationService@aku.edu");
-                mail.To.Add("umer.nasir@aku.edu");
-                mail.Subject = "Test Mail";
-                mail.Body = "This is for testing SMTP mail from GMAIL";
+                    message.From = fromAddress;
+                    message.Subject = "test";
+                    // Set IsBodyHtml to true means you can send HTML email.
+                    message.IsBodyHtml = true;
+                    message.Body = "<h1>Test</h1>";
+                    message.To.Add("umer.nasir@aku.edu");
 
-                SmtpServer.Port = 587;
-                SmtpServer.Credentials = new System.Net.NetworkCredential("username", "password");
-                SmtpServer.EnableSsl = true;
-
-                SmtpServer.Send(mail);
-                return true;
+                    try
+                    {
+                        smtpClient.Send(message);
+                    }
+                    catch (Exception ex)
+                    {
+                        //Error, could not send the message
+                       // Response.Write(ex.Message);
+                    }
+                }
             }
-            catch(Exception ex)
-            {
-                return false;
-            }
+            //try
+            //{
+
+
+            //    MailMessage mail = new MailMessage();
+            //    SmtpClient SmtpServer = new SmtpClient("exmbxcas4.aku.edu");
+
+            //    mail.From = new MailAddress("umer.nasir@aku.edu");
+            //    mail.To.Add("umer.nasir@aku.edu");
+            //    mail.Subject = "Test Mail";
+            //    mail.Body = "This is for testing SMTP mail from GMAIL";
+
+            //    SmtpServer.Port = 25;
+            //    SmtpServer.Credentials = new System.Net.NetworkCredential("umer.nasir@aku.edu", "qwerty1.");
+            //    SmtpServer.EnableSsl = true;
+
+            //    SmtpServer.Send(mail);
+            //    return true;
+            //}
+            //catch(Exception ex)
+            //{
+            //    return false;
+            //}
         }
 
         public ActionResult ViewTemplete()
